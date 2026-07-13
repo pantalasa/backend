b76964d54e712bc1e55f7edc6b2e2d01cb30b898
Brandon Schurman <brandon@earthly.dev>
2026-07-13 13:04:58 -0400

    Add debug log to /healthz probe

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		logrus.Debug("healthz probe")
 		fmt.Fprint(w, "ok")
 	})
 
